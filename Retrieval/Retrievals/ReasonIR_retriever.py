84b8cbd03a775581f31c4e0810d17adb89c79435
ZeinabTaghavi <zeinabtaghavi1377@gmail.com>
2025-06-13 21:59:53 +0200

    updated

--- Retrieval/Retrievals/ReasonIR_retriever.py
+++ Retrieval/Retrievals/ReasonIR_retriever.py
@@ -30,13 +30,17 @@ class ReasonIRRetriever:
         self.k = k
         self.llm_model_name = llm_model_name
         self.model_kwargs = {"torch_dtype": "auto", 'device_map': "auto"}
-
+        
+        # Get the device of the model
         self.model = SentenceTransformer(self.llm_model_name, trust_remote_code=True, model_kwargs=self.model_kwargs)
+        self.device = next(self.model.parameters()).device
 
         self.model.set_pooling_include_prompt(include_prompt=False) # exclude the prompt during pooling
 
         doc_instruction = ""
         self.doc_embeddings = [self.model.encode(doc, instruction=doc_instruction) for doc in self.corpus]
+        # Move all embeddings to the same device as the model
+        self.doc_embeddings = [torch.tensor(emb, device=self.device) for emb in self.doc_embeddings]
 
     def retrieve_data(self, query: str):
         """
@@ -48,6 +52,8 @@ class ReasonIRRetriever:
         # perform retrieval for one query
         query_instruction = ""
         query_embedding = self.model.encode(query, instruction=query_instruction)
+        # Move query embedding to the same device as the model
+        query_embedding = torch.tensor(query_embedding, device=self.device)
         scores = [query_embedding @ doc_embedding.T for doc_embedding in self.doc_embeddings]
         top_docs = [self.corpus[i] for i in np.argsort(scores)[::-1][:self.k]]
         top_scores = [scores[i] for i in np.argsort(scores)[::-1][:self.k]]
